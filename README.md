2f76cd4b4e8e55174eab03d4c2ecb053d4288465
Robert Knight <rdknight@gmail.com>
2024-06-03 15:51:37 +0100

    Now a cyclic function to decode json to binary and back

--- README.md
+++ README.md
@@ -11,11 +11,23 @@ Needed to install rust and maturin.  pyo3 is a rust module and becomes available
 `matturin develop` - to build
 
 The very first function is now working and can be tested as follows:
+## Usage
+
+Using ChatGPT I have updated this dump to binary and go back the other way.  This now needs changing to make use of the packed-struct library which allows specification of the correct bits.
+When compiling I moved the `unpackerrs` directory out of the way so that the defualt libraries would appear
+
 ```python
 Python 3.11.8 (main, Feb 26 2024, 21:39:34) [GCC 11.2.0] on linux
 Type "help", "copyright", "credits" or "license" for more information.
 >>> import unpackerrs
->>> unpackerrs.sum_as_string(3,4)
-'7'
+>>> dir(unpackerrs)
+['__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'deserialize_from_binary', 'serialize_to_binary', 'unpackerrs']
+>>> json_str = '{"name": "Alice", "age": 30}'
+>>> binary_data = unpackerrs.serialize_to_binary(json_str)
+>>> print(binary_data)
+[5, 0, 0, 0, 0, 0, 0, 0, 65, 108, 105, 99, 101, 30, 0, 0, 0]
+>>> reconstructed_json_str = unpackerrs.deserialize_from_binary(binary_data)
+>>> print(reconstructed_json_str)
+{"name":"Alice","age":30}
 >>>
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,66 @@
+use packed_struct::prelude::*;
 use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use bincode;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[derive(serde::Serialize, serde::Deserialize, PackedStruct)]
+#[packed_struct(bit_numbering="msb0")]
+pub struct TestPack {
+    #[packed_field(bits="0..=2")]
+    tiny_int: Integer<u8, packed_bits::Bits::<3>>,
+    #[packed_field(bits="3")]
+    enabled: bool
 }
+
+#[derive(serde::Serialize, serde::Deserialize, PackedStruct)]
+#[packed_struct(bit_numbering = "msb0", size_bytes = "12", endian = "lsb")]
+pub struct Vec3 {
+    pub x: u32,
+    pub y: u32,
+    pub z: u32,
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct MyData {
+    // Define your data structure
+    name: String,
+    age: u32,
+}
+
+#[pyfunction]
+fn deserialize_from_binary(binary_data: Vec<u8>) -> PyResult<String> {
+    // Deserialize binary data to Rust struct
+    let data: MyData = bincode::deserialize(&binary_data)
+        .map_err(|e| PyValueError::new_err(format!("Failed to deserialize binary data: {}", e)))?;
+
+    // Serialize struct to JSON string
+    let json_str = serde_json::to_string(&data)
+        .map_err(|e| PyValueError::new_err(format!("Failed to serialize to JSON: {}", e)))?;
+
+    Ok(json_str)
+}
+
+#[pyfunction]
+fn serialize_to_binary(json_str: &str) -> PyResult<Vec<u8>> {
+    // Deserialize JSON string to Rust struct
+    let data: MyData = serde_json::from_str(json_str)
+        .map_err(|e| PyValueError::new_err(format!("Failed to deserialize JSON: {}", e)))?;
+
+    // Serialize struct to binary
+    let encoded: Vec<u8> = bincode::serialize(&data)
+        .map_err(|e| PyValueError::new_err(format!("Failed to serialize to binary: {}", e)))?;
+
+    Ok(encoded)
+}
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn unpackerrs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(deserialize_from_binary, m)?)?;
+    m.add_function(wrap_pyfunction!(serialize_to_binary, m)?)?;
     Ok(())
 }
